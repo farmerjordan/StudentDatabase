ec6dcdef97224d740e4b125052facdf10c80ccff
eralbrecht <60952785+eralbrecht@users.noreply.github.com>
2020-05-08 19:15:49 -0700

    add student options

--- main.cpp
+++ main.cpp
@@ -6,5 +6,19 @@ using namespace std;
 
 main()
 {
-
+	cout<<"Menu - please type a number"<<endl;
+	cout<<"1 -- print all students and their info"<<endl;
+	cout<<"2 -- print all faculty and their info"<<endl;
+	cout<<"3 -- print student information from a provided ID"<<endl;
+	cout<<"4 -- print faculty information from a provided ID"<<endl;
+	cout<<"5 -- print name and info of advisor from provided student ID"<<endl;
+	cout<<"6 -- print all advisee information given advisor ID"<<endl;
+	cout<<"7 -- add a student"<<endl;
+	cout<<"8 -- delete a student from a given ID#"<<endl;
+	cout<<"9 -- add a faculty member"<<endl;
+	cout<<"10 -- delete a faculty member from a given ID#"<<endl;
+	cout<<"11 -- change a students advisor given student ID# and new advisor ID#">>endl;
+	cout<<"12 -- remove an advisee from a faculty member from student and faculty ID#'s"<<endl;
+	cout<<"13 -- undo last action"<<endl;
+	cout<<"14 -- exit program"<<endl;
 }
